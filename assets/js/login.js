f343c3cd8201b8998c15b6dca4cb24c8d1cc291b
Nope0801 <hoviettung2@gmail.com>
2024-10-29 20:49:07 +0700

    Commit lần 4 gần xong web

--- assets/js/login.js
+++ assets/js/login.js
@@ -40,3 +40,77 @@ registerContainer.addEventListener('click', function(event){
     event.stopPropagation();
 })
 
+const email1 = document.getElementById('email1');
+const password1 = document.getElementById('password1');
+const surname = document.getElementById('surname');
+const name1 = document.getElementById('name');
+const accountName = document.getElementById('accountName');
+const number = document.getElementById('number');
+const email2 = document.getElementById('email2');
+const password2 = document.getElementById('password2');
+const confirmPassword = document.getElementById('confirmPassword');
+
+const loginBtn2 = document.getElementsByClassName('js-login-btn')[1];
+const registerBtn2 = document.getElementsByClassName('js-register-btn')[1];
+
+function login1(){
+    if(email1.value.trim() === ""|| password1.value.trim() === ""){
+        alert("Vui lòng điền đầy đủ thông tin");
+        return;
+    }
+    else if(validateEmail(email1.value.trim()) === false){
+        alert("Email không phù hợp");
+        return;
+    }
+    else if(validatePassword(password1.value.trim()) === false){
+        alert("Mật khẩu không phù hợp");
+        return;
+    }
+}
+
+function register1(){
+    if(surname.value.trim() === "" || name1.value.trim() === ""|| accountName.value.trim() === ""|| number.value.trim() ===""||email2.value.trim() ===""|| password2.value.trim() ===""||confirmPassword.value.trim() ===""){
+        alert("Vui lòng điền đầy đủ thông tin");
+        return;
+    }
+    else if(validateEmail(email2.value.trim()) === false){
+        alert("Email không phù hợp");
+        return;
+    }
+    else if(validatePassword(password2.value.trim()) === false){
+        alert("Mật khẩu không phù hợp");
+        return;
+    }
+    else if(validateNumber(number.value.trim()) === false){
+        alert("Số điện thoại không phù hợp");
+        return;
+    }
+    else if(confirmPassword.value.trim() != password2.value.trim()){
+        alert("Mật khẩu xác nhận không phù hợp");
+        return;
+    }
+}
+
+
+function validateEmail(email){
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/;
+    return emailRegex.test(email);
+}
+
+function validateNumber(number){
+    const numberRegex = /^\+?(\d{1,4})?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/;
+    return numberRegex.test(number);
+}
+
+function validateName(name){
+    const nameRegex =  /^[a-zA-Z\s]+$/;
+    return nameRegex.test(name);
+}
+function validatePassword(password){
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+
+loginBtn2.addEventListener('click', login1);
+registerBtn2.addEventListener('click', register1);
